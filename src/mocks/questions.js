92ae8e689310fcae2f2134cfce78212349fa2cf7
Alexander <al.antonov@contact-sys.com>
2019-10-22 18:19:55 +0300

    module3-task1

---
+++ src/mocks/questions.js
@@ -0,0 +1,25 @@
+const questions = [
+    {
+        type: `genre`,
+        genre: `rock`,
+        answers: [
+            {
+                src: `https://******.mp3`,
+                genre: `rock`,
+            },
+            {
+                src: `https://******.mp3`,
+                genre: `pop`,
+            },
+            {
+                src: `https://******.mp3`,
+                genre: `jazz`,
+            },
+            {
+                src: `https://******.mp3`,
+                genre: `rock`,
+            },                                    
+        ],
+    },
+];
+
